55cd2a95749cd2349e522ca22a8e0efa63b5947f
Elvertoni Coimbra <elvertoni@gmail.com>
2023-11-19 16:01:21 -0300

    primeiro commit

---
+++ back/Models/Evento.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+
+namespace proeventos.back.Models;
+public class Evento
+{
+    public int EventoId { get; set; }
+    public string Local { get; set; }
+    public string DataEvento { get; set; }
+    public string Tema { get; set; }
+    public int QtdPessoas { get; set; }
+    public string Lote { get; set; }
+    public string ImagemURL { get; set; }
+
+}
